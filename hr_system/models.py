232f8accbb7be0df3c30095b3a78bfaa7be8df77
Richard Fong <richard11767@hotmail.com>
2022-10-13 15:54:27 +0800

    add clock in function

--- hr_system/models.py
+++ hr_system/models.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 from django.urls import reverse
+import datetime as dt
 
 # Create your models here.
 
@@ -22,3 +23,41 @@ class EmployeeAdmin(admin.ModelAdmin):
 
     fields = ["first_name", "last_name", "email", "department", "username"] # 顯示欄位
     search_fields = ('first_name','last_name') # 搜尋欄位
+
+class ClockInRecord(models.Model):
+    date = models.DateField(auto_now_add=True)
+    morning_enter_time = models.TimeField(null=True)
+    morning_leave_time = models.TimeField(null=True)
+    afteroon_enter_time = models.TimeField(null=True)
+    afteroon_leave_time = models.TimeField(null=True)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, to_field="username", db_column="username")
+
+    def clock_in(self, datetime):
+        if dt.time(17,30,0) <= datetime.time():
+            if self.afteroon_leave_time == None:
+                self.afteroon_leave_time = datetime
+                return True
+            return False
+        elif dt.time(13,30,0) <= datetime.time():
+            if self.afteroon_enter_time == None:
+                self.afteroon_enter_time = datetime
+                return True
+            return False
+        elif dt.time(13,0,0) <= datetime.time():
+            if self.morning_leave_time == None:
+                self.morning_leave_time = datetime
+                return True
+            return False
+        elif dt.time(8,30,0) <= datetime.time():
+            if self.morning_enter_time == None:
+                self.morning_enter_time = datetime
+                return True
+            return False
+
+
+@admin.register(ClockInRecord)
+class ClockInRecordAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ClockInRecord._meta.fields]
+
+    fields = ["date", "morning_enter_time", "morning_leave_time", "afteroon_enter_time", "afteroon_leave_time", "username"] # 顯示欄位
+    search_fields = ('date','username') # 搜尋欄位
--- hr_system/views.py
+++ hr_system/views.py
@@ -1,25 +1,59 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Employee
+from .models import Employee, ClockInRecord
 from .forms import EmployeeForm
 from django.contrib import messages
+from datetime import datetime
+import pytz
 
 # Create your views here.
 
 
 @login_required(login_url="login")
 def hr_clockin(request):
+    timezone = pytz.timezone('Asia/Shanghai')
+    current_datetime = datetime.now(timezone)
+    current_date = current_datetime.date()
+    current_time = current_datetime.time()
+    current_date_str = current_datetime.strftime("%m/%d/%Y")
+    current_time_str = current_datetime.strftime("%H:%M:%S")
+
     username = request.user.username
     employee = Employee.objects.filter(username=username).values()
+
     if list(employee):
         employee_dict = list(employee)[0]
-    # else:
+        clock_in_msg = ''
+        this_month_clock_in_record = ClockInRecord.objects.filter(
+            date__year = current_date.year,
+            date__month = current_date.month,
+            username=User.objects.get(username=username)
+        )
 
-    context = {
-        'employee': employee
-    }
-    return render(request, "hrs/hr_clockin.html", context)
+        if request.method == 'POST':
+            today_clock_in_record, _ = ClockInRecord.objects.get_or_create(
+                date=current_date, 
+                username=User.objects.get(username=username)
+            )
+            clock_in_msg = "Successfully clock in, time: " + current_time_str
+
+            if today_clock_in_record.clock_in(current_datetime):
+                clock_in_msg = "Successfully clock in, time: " + current_time_str
+            else:
+                clock_in_msg = "Fail to clock in."
+            today_clock_in_record.save()
+
+        context = {
+            'clock_in_record': this_month_clock_in_record,
+            'employee': employee_dict,
+            'clock_in_msg': clock_in_msg,
+        }
+        return render(request, "hrs/hr_clockin.html", context)
+    else:
+        messages.error(request, "Please fill in the profile first!")
+        return redirect("hr:hr_profile")
+        
 
 @login_required(login_url="login")
 def hr_profile(request):
